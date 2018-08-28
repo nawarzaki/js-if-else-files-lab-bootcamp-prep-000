a530bf949a7d23a4e70585670f95537e81fd79c5
nawaar.zakee@yahoo.com <nawaar.zakee@yahoo.com>
2018-08-28 12:56:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ if (name === "Susan") {
 module.exports = { name, height
 }
 var hieght = "74"
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
