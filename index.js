d6c8593f33d7a0065ae43b4a0981d446518b65f6
nawaar.zakee@yahoo.com <nawaar.zakee@yahoo.com>
2018-08-28 12:55:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var name = "Joe"
 var height = "74"
+if (name === "Susan") {
+    console.log("The name is correct")
+}
 
 
 // Don't worry about this
 module.exports = { name, height
 }
 var hieght = "74"
-
